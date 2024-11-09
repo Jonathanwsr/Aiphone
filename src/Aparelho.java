45e4fe2e391a65067371e5646bf0031a6e3ace74
Jonathan <jonathanw.rocha@unifacol.edu.br>
2024-11-09 17:29:15 -0300

    Adicionando a classe Apareçho onde ela vai exibir as informaçoes do aparelho.

--- src/Aparelho.java
+++ src/Aparelho.java
@@ -1,2 +1,66 @@
 public class Aparelho {
+
+    private String marca;
+    private String modelo;
+    private double preco;
+    private String appMusica;
+    private String appNavegador;
+    private String appLigacao;
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setAppMusica(String appMusica) {
+        this.appMusica = appMusica;
+    }
+
+    public String getAppMusica() {
+        return appMusica;
+    }
+
+    public void setAppNavegador(String appNavegador) {
+        this.appNavegador = appNavegador;
+    }
+
+    public String getAppNavegador() {
+        return appNavegador;
+    }
+
+    public void setAppLigacao(String appLigacao) {
+        this.appLigacao = appLigacao;
+    }
+
+    public String getAppLigacao() {
+        return appLigacao;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Preço: R$" + preco);
+        System.out.println("App de Música: " + appMusica);
+        System.out.println("App de Navegação: " + appNavegador);
+        System.out.println("App de Ligação: " + appLigacao);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
 
+        Aparelho meuAparelho = new Aparelho();
+
+        meuAparelho.setMarca("Apple");
+        meuAparelho.setModelo("iPhone 15");
+        meuAparelho.setPreco(15000.0);
+        meuAparelho.setAppMusica("Spofy");
+        meuAparelho.setAppLigacao("Chamada");
+        meuAparelho.setAppNavegador("Google");
+
+
+
+        meuAparelho.exibirInformacoes();
+
+
+
+
         ReprodutorMusica.TipoMusica();
         ReprodutorMusica.TocarMusica();
         ReprodutorMusica.trocarMusica();
